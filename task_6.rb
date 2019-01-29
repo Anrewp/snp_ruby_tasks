3b8f0f3b378feeaa70a663988c667a0e24b5313a
Anrewp <46995871+Anrewp@users.noreply.github.com>
2019-01-29 19:00:18 +0300

    Update task_6.rb

--- task_6.rb
+++ task_6.rb
@@ -11,51 +11,52 @@
 # • в иных случаях необходимо вернуть имя и ход победителя, если оба
 # игрока походили одинаково - выигрывает первый игрок.
 
+class WrongNumberOfPlayersError < ArgumentError ; end
+class NoSuchStrategyError < NameError ; end
+
 # Cheking plyer strategy
 def rps_game_check(player)
   str = player.join(' ')
-  raise NameError unless /player\s*\d+\s+[RPS]/.match?(str)
+  raise NoSuchStrategyError unless /player\s*\d+\s+[RPS]/.match?(str)
 end
 
-begin
-  # Takes two args and returns winner
-  def rps_game_winner(*players)
-    raise ArgumentError unless players.length == 2
+# Takes two args and returns winner
+def rps_game_winner(players)
+  raise WrongNumberOfPlayersError unless players.length == 2
 
-    players.each { |player| rps_game_check(player) }
+  players.each { |player| rps_game_check(player) }
 
-    strategy = []
-    players.each { |player| strategy.push(player[1]) }
+  strategy = []
+  players.each { |player| strategy.push(player[1]) }
 
-    case strategy.join
-    when /RR|SS|PP|PR|RS|SP/
-      players[0].join(' ')
-    else players[1].join(' ')
-    end
-
-  rescue ArgumentError
-    'WrongNumberOfPlayersError'
-  rescue NameError
-    'NoSuchStrategyError'
+  case strategy.join
+  when /RR|SS|PP|PR|RS|SP/
+    players[0].join(' ')
+  else players[1].join(' ')
   end
+
+  rescue WrongNumberOfPlayersError
+    'WrongNumberOfPlayersError'
+  rescue NoSuchStrategyError
+    'NoSuchStrategyError'
 end
 
 # TESTS
 
-# if rps_game_winner(%w[player1 P], %w[player2 S], %w[player3 S]) ==
+# if rps_game_winner([%w[player1 P], %w[player2 S], %w[player3 S]]) ==
 #   "WrongNumberOfPlayersError"
 #   puts "true"
 # else puts "   FALSE 1"
 # end
-# if rps_game_winner(%w[player1 P], %w[player2 A]) == "NoSuchStrategyError"
+# if rps_game_winner([%w[player1 P], %w[player2 A]]) == "NoSuchStrategyError"
 #   puts "true"
 # else puts "   FALSE 2"
 # end
-# if rps_game_winner(%w[player1 P], %w[player2 S]) == 'player2 S'
+# if rps_game_winner([%w[player1 P], %w[player2 S]]) == 'player2 S'
 #   puts "true"
 # else puts "   FALSE 3"
 # end
-# if rps_game_winner(%w[player1 P], %w[player2 P]) == 'player1 P'
+# if rps_game_winner([%w[player1 P], %w[player2 P]]) == 'player1 P'
 #   puts "true"
 # else puts "   FALSE 4"
 # end
