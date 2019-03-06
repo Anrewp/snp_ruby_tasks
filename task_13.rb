73d883602b85c3aab0f7477f01a129e42ec8150a
Anrewp <46995871+Anrewp@users.noreply.github.com>
2019-03-06 14:09:44 +0300

    Update task_13.rb

--- task_13.rb
+++ task_13.rb
@@ -1,15 +1,19 @@
 # Адаптируйте ваше решение из Упражнения 1 так, чтобы вместо palindrome?("foo")
 # вы могли его использовать как "foo".palindrome? # => false.
 
-class String
+module Palindrome
+
   def palindrome?
     return false if nil?
 
     str = downcase.scan(/\w/)
-    return true if str == str.reverse
+    str == str.reverse
 
-    false
   end
+end
+
+class String
+  include Palindrome
 end
 
 # TESTS
